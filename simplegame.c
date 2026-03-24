258112b0f4e59a772f4f579c73b3f8915eda71c5
NoahHasselmann <nh.studios@yahoo.com>
2026-03-24 14:00:15 +0100

    feat: make great
    Co-Authored-By: Piet <piet.unger@gmail.com>

--- simplegame.c
+++ simplegame.c
@@ -2,30 +2,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-
-int main() {
+int main()
+{
     srand(time(NULL));
 
     int number;
     int correct_number = (rand() % 100) + 1;
 
-    while (1) {
+    int counter = 0;
+
+    while (1)
+    {
         printf("Input your number: \n");
-        scanf("%d", &number);
-    
+        if (!scanf("%d", &number))
+        {
+            printf("That is not a number!\n");
+            continue;
+        }
+        
+        counter++;
         printf("The number is ");
-        if (number > correct_number) {
+        if (number > correct_number)
+        {
             printf("lower! \n");
         }
-        else if (number < correct_number) {
+        else if (number < correct_number)
+        {
             printf("higher! \n");
         }
-        else {
-            printf("Correct number! Why? Why did you do this?");
+        else
+        {
+            printf("Correct! You guessed the number in %d attempt(s).\n", counter);
             break;
         }
     }
-
+    
     return 0;
-
 }
